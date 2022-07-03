71e7f33861f573790c0e9534a2dcc88a3499dd70
corvotorvo <83085402+MAlcont@users.noreply.github.com>
2022-07-03 11:23:55 +0200

    Completed module

--- cms/admin/auth.py
+++ cms/admin/auth.py
@@ -10,3 +10,18 @@ def protected(route_function):
             return redirect(url_for('admin.login'))
         return route_function(**kwargs)
     return wrapped_route_function
+
+@admin_bp.before_app_request
+def load_user():
+    user_id= session.get('user_id')
+    g.user= User.query.get(user_id) if user_id is not None else None
+
+@admin_bp.route('/login', methods=("GET", "POST:"))
+def login():
+    if request.method == "POST":
+        username= request.form['username']
+        password= request.form['password']
+
+        error= None
+
+    return render_template('admin/login.html')
